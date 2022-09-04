5132321351b8568ecfacf9deff621c8ef0b96142
TAH4uk <pcs_zero@inbox.ru>
2022-09-04 22:11:29 +0300

    zadachi

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] array = new int[size];
+
+// ArrayFindPositiveNumber(array);
+// Console.WriteLine("Полученный массив: ");
+// PrintArray(array);
+
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] % 2 == 0)
+//         {
+//             count++;
+//         }
+// }
+
+// Console.WriteLine($"Всего чисел в массиве {array.Length}, {count} из них чётные");
+
+// void ArrayFindPositiveNumber(int[] numbers)
+// {
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100, 1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
