a1d79d8bb49dfac994ba2b02cf1c951931a38877
TAH4uk <pcs_zero@inbox.ru>
2022-09-04 23:41:57 +0300

    zadachi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// Console.WriteLine("Введите размер массива");
+// Console.Write("Введите размер массива");
 // int size = Convert.ToInt32(Console.ReadLine());
 
 // int[] array = new int[size];
@@ -19,7 +19,7 @@
 //         }
 // }
 
-// Console.WriteLine($"Всего чисел в массиве {array.Length}, {count} из них чётные");
+// Console.WriteLine($"Количество четных чисел в полученном массиве - {count}");
 
 // void ArrayFindPositiveNumber(int[] numbers)
 // {
@@ -28,6 +28,7 @@
 //         numbers[i] = new Random().Next(100, 1000);
 //     }
 // }
+
 // void PrintArray(int[] numbers)
 // {
 //     Console.Write("[ ");
@@ -40,3 +41,124 @@
 // }
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// Console.Write("Введите размер массива - ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] numbers = new int[size];
+// ArrayRandomNum(numbers);
+// Console.WriteLine("Полученный массив: ");
+// PrintArray(numbers);
+
+// int sum = 0;
+
+// for (int i = 0; i < numbers.Length; i+=2)
+// {
+//     sum = sum + numbers[i];
+// }
+//     Console.WriteLine($"Сумма элементов на нечётных позициях полученного массива - {sum}");
+
+// void ArrayRandomNum(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,100);
+//         }
+// }
+
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+
+Console.Write("Введите размер массива - ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+ArrayRandomNum(numbers);
+Console.WriteLine("Полученный массив: ");
+PrintArray(numbers);
+
+int comp = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    comp = comp + numbers[i];
+}
+    Console.WriteLine($"Сумма элементов на нечётных позициях полученного массива - {sum}");
+
+void ArrayRandomNum(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// Console.Write("Введите размер массива - ");
+// int size = Convert.ToInt32(Console.ReadLine());
+
+// int[] numbers = new int[size];
+// ArrayRandomNum(numbers);
+// Console.WriteLine("Полученный массив: ");
+// PrintArray(numbers);
+
+// double maxNumb = numbers[0];
+// double minNumb = numbers[0];
+
+// for (int i = 1; i < numbers.Length; i++)
+// {
+//     if (maxNumb < numbers[i])
+//     {
+//         maxNumb = numbers[i];
+//     }
+//     if (minNumb > numbers[i])
+//     {
+//         minNumb = numbers[i];
+//     }
+// }
+
+// double diff = maxNumb - minNumb;
+
+// Console.WriteLine($"Hазница между максимальным ({maxNumb}) и минимальным ({minNumb}) элементами: {diff}");
+
+// void ArrayRandomNum(int[] numbers)
+// {
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(1, 10);
+//     }
+// }
+
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
