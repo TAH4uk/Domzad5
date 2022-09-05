8b185723d9c3b9b5dfb76ba99f7b9819ed9f5d87
TAH4uk <pcs_zero@inbox.ru>
2022-09-05 09:12:10 +0300

    zadachi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// Console.Write("Введите размер массива");
+// Console.Write("Введите размер массива - ");
 // int size = Convert.ToInt32(Console.ReadLine());
 
 // int[] array = new int[size];
@@ -52,7 +52,7 @@
 
 // int sum = 0;
 
-// for (int i = 0; i < numbers.Length; i+=2)
+// for (int i = 1; i < numbers.Length; i+=2)
 // {
 //     sum = sum + numbers[i];
 // }
@@ -62,7 +62,7 @@
 // {
 //     for(int i = 0; i < numbers.Length; i++)
 //         {
-//             numbers[i] = new Random().Next(1,100);
+//             numbers[i] = new Random().Next(-10,10);
 //         }
 // }
 
@@ -80,46 +80,6 @@
 // Задача 37: Найдите произведение пар чисел в одномерном массиве. 
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 
-Console.Write("Введите размер массива - ");
-int size = Convert.ToInt32(Console.ReadLine());
-
-int[] numbers = new int[size];
-ArrayRandomNum(numbers);
-Console.WriteLine("Полученный массив: ");
-PrintArray(numbers);
-
-int comp = 0;
-
-for (int i = 0; i < numbers.Length; i++)
-{
-    if(i % 2 == 0)
-    {
-        comp = numbers[i] * numbers[size-1];
-    }
-}
-    Console.WriteLine($"Произведение пар чисел - {comp}");
-
-void ArrayRandomNum(int[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,10);
-        }
-}
-
-void PrintArray(int[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
-
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-
 // Console.Write("Введите размер массива - ");
 // int size = Convert.ToInt32(Console.ReadLine());
 
@@ -128,31 +88,23 @@ void PrintArray(int[] numbers)
 // Console.WriteLine("Полученный массив: ");
 // PrintArray(numbers);
 
-// double maxNumb = numbers[0];
-// double minNumb = numbers[0];
-
-// for (int i = 1; i < numbers.Length; i++)
+// int comp = 0;
+// // 
+// for (int i = 0; i < numbers.Length;  i++)
 // {
-//     if (maxNumb < numbers[i])
+//     if(i < size / 2)
 //     {
-//         maxNumb = numbers[i];
-//     }
-//     if (minNumb > numbers[i])
-//     {
-//         minNumb = numbers[i];
+//         comp = numbers[i] * numbers[size-1-i];
+//         Console.WriteLine($"Произведение пар чисел - {comp}");
 //     }
 // }
 
-// double diff = maxNumb - minNumb;
-
-// Console.WriteLine($"Hазница между максимальным ({maxNumb}) и минимальным ({minNumb}) элементами: {diff}");
-
 // void ArrayRandomNum(int[] numbers)
 // {
-//     for (int i = 0; i < numbers.Length; i++)
-//     {
-//         numbers[i] = new Random().Next(1, 10);
-//     }
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = new Random().Next(1,10);
+//         }
 // }
 
 // void PrintArray(int[] numbers)
@@ -165,3 +117,51 @@ void PrintArray(int[] numbers)
 //     Console.Write("]");
 //     Console.WriteLine();
 // }
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Write("Введите размер массива - ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] numbers = new double[size];
+ArrayRandomNum(numbers);
+Console.WriteLine("Полученный массив: ");
+PrintArray(numbers);
+
+double maxNumb = numbers[0];
+double minNumb = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (maxNumb < numbers[i])
+    {
+        maxNumb = numbers[i];
+    }
+        if (minNumb > numbers[i])
+        {
+            minNumb = numbers[i];
+        }
+}
+
+double diff = maxNumb - minNumb;
+
+Console.WriteLine($"Разница между максимальным ({maxNumb}) и минимальным ({minNumb}) элементами: {diff}");
+
+void ArrayRandomNum(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-10, 10);
+    }
+}
+
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
